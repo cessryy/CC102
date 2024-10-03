3ce839d9bfe94a285c6f18d1aa4b753b356a144d
cessryy <icatlosarry2@gmail.com>
2024-10-03 11:13:20 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string Nname, yL, school;
+	cout << "Nickname: ";
+	cin >> Nname;
+	cout << "Course-Year Level: ";
+	cin >> yL;
+	cout << "School: ";
+	cin >> school;
+	cout << "Wow Congrats " << Nname << "! " << yL << " is a nice course. And you are studying in " << school << " which is one of the Center of Excellence in Tertiary Education."; 
+	return 0;
+}

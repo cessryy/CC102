2c8c7ee62a25c40a0e04357539d29bcdb22d8d43
cessryy <icatlosarry2@gmail.com>
2024-10-03 10:35:44 +0800

    Add files via upload

---
+++ 02Basic_Output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+	
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION";
+	cout << "College of Computer Studies";
+	cout << "BSIT-1A";
+	cout << "Bryan Joshua C. Malvarosa";
+	cout << "P-7a, Sitio Cambulong, Brgy. Basiad, Sta. Elena, Cam. norte";
+	return 0;
+}
